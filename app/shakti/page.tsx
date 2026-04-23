6308f6dd564400e837ab908e485a6b3fc64c4761
MarcelDWill <truu2u@yahoo.com>
2026-04-23 09:42:46 -0400

    Fix formatting errors and typos in Shakti page

--- app/shakti/page.tsx
+++ app/shakti/page.tsx
@@ -73,17 +73,45 @@ export default function ShaktiPage() {
 
           <div className="mt-12 max-w-2xl mx-auto border-t border-gray-200 dark:border-gray-700 pt-8">
              <p className="text-lg md:text-xl text-gray-700 dark:text-gray-300 font-medium leading-relaxed tracking-wide px-4 mb-6">
-               &quot;I wasn&apos;t sure what to say. Hi. I don&apos;t want to come off too strong or weird. Let me apologize for coming out of nowhere sending you a cactus and we havent spoken in almost 30years. As a psychatrist you will understand. You are registered in my brain as a safe space, my only safe space. Once again I apologize. If you want mdwilliams326@gmail.com or 3174966590.&quot;
+               &quot;I wasn&apos;t sure what to say. Hi. I don&apos;t want to come off too strong or weird. Let me apologize for coming out of nowhere sending you a cactus and we haven&apos;t spoken in almost 30 years. 
+               As a psychiatrist you will understand. You are registered in my brain as a safe space, my only safe space. Once again I apologize. If you want mdwilliams326@gmail.com or 3174966590.&quot;
              </p>
              <p className="text-sm md:text-base text-gray-500 dark:text-gray-400 italic px-4">
-               PS * = At this point Im writing this for me. I realized at some point that I really wanted to be with you. And I know I never wanted to break up with you. I never thought anything positive about my future. But my hurt ran deep and I restructured my emotions. I became a Emotionaly Detached Operator and a Deep Internal Observer. The memory of you became the funnel or stopgap. Most of my creative endeavors started with a memory of you. At the same time I could shut off any emotion for anyone in life, family included. But because of something your memory was the only place I let emotions floww freely. My ID clung to you because I didnt let go of how I felt, I believe you were the first person to honestly encourage me. My rational brain lost out to the impulse of sending you that cactus. I recognized the closer it got to being delivered the reality that I don't know who you became. In some ways I don't think I did because I wanted to resemble how I thought you wouldve remembered me. Truly for 30yrs Ive held onto hope for a reunion and its crazy like a lifetime movie. But now I think I can move on, I think sending the cactus was that kid in me last shot. Once it felt like it did what it could do, it sat down. Going forward for me, Im not sure. When I write you were my actual muse, I wrote with the intention of you reading it. Thats not realistic, so Im wondering how my writing will be. I am melancholic right now with reality settling so maybe Ill hold on to that feeling for a while and use it. ;
+               PS * = At this point I&apos;m writing this for me. I realized at some point that I really wanted to be with you. 
+               And I know I never wanted to break up with you. I never thought anything positive about my future. 
+               But my hurt ran deep and I restructured my emotions. I became an Emotionally Detached Operator and a Deep Internal Observer. 
+               The memory of you became the funnel or stopgap. Most of my creative endeavors started with a memory of you.
+                At the same time I could shut off any emotion for anyone in life, family included. 
+                But because of something your memory was the only place I let emotions flow freely. 
+                My ID clung to you because I didn&apos;t let go of how I felt, I believe you were the first person to honestly encourage me. 
+                My rational brain lost out to the impulse of sending you that cactus. 
+                I recognized the closer it got to being delivered the reality that I don&apos;t know who you became. 
+                In some ways I don&apos;t think I did because I wanted to resemble how I thought you would&apos;ve remembered me.
+                 
              </p>
              <p className="text-sm md:text-base text-gray-500 dark:text-gray-400 italic px-4">
-               PPS I never doubted before to now how I felt about you. Never questioned it. The silver necklace you gave me became a symbol of my heart. I never take it off but I have to now. To satisfy the kid in me I will leave this page here. Maybe Ill add to it occasionally, like a hidden journal only shared with the memory of you, that should satify it. Be well and happyI wonder if you&apos;ll ever read this and if you still like your PS&apos;s
+               Truly for 30 years I&apos;ve held onto hope for a reunion and it&apos;s crazy like a lifetime movie. 
+                 But now I think I can move on, I think sending the cactus was that kid in me&apos;s last shot. 
+                 Once it felt like it did what it could do, it sat down. Going forward for me, I&apos;m not sure. 
+                 When I write you were my actual muse, I wrote with the intention of you reading it. 
+                 That&apos;s not realistic, so I&apos;m wondering how my writing will be. 
+                 I am melancholic right now with reality settling so maybe I&apos;ll hold on to that feeling for a while and use it.
+             </p>
+             <p className="text-sm md:text-base text-gray-500 dark:text-gray-400 italic px-4">
+               PPS I never doubted before to now how I felt about you. Never questioned it. 
+               The silver necklace you gave me became a symbol of my heart. 
+               I never take it off but I have to now.
+                To satisfy the kid in me I will leave this page here. 
+                Maybe I&apos;ll add to it occasionally, like a hidden journal only shared with the memory of you, that should satisfy it. 
+                Be well and happy. I wonder if you&apos;ll ever read this and if you still like your PS&apos;s.
+             </p>
+             <p className="text-sm md:text-base text-gray-500 dark:text-gray-400 italic px-4">
+
              </p>
              <p className="text-sm md:text-base text-gray-500 dark:text-gray-400 italic px-4">
                PPPS Happy belated birthday x30 or is it 29
              </p>
+
           </div>
         </div>
       </div>
